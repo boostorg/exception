6cf231330acc3f697faf5a61a592c1c7e548018a
Emil Dotchevski <emil@revergestudios.com>
2016-10-26 12:35:25 -0700

    Ticket #12059

--- test/error_info_lv_const_test.cpp
+++ test/error_info_lv_const_test.cpp
@@ -3,7 +3,7 @@
 //Distributed under the Boost Software License, Version 1.0. (See accompanying
 //file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-#include "boost/exception/info.hpp"
+#include <boost/exception/info.hpp>
 template <class E,class I>
 E const &
 add_info( E const & e, I const & i )
--- test/error_info_rv_test.cpp
+++ test/error_info_rv_test.cpp
@@ -3,7 +3,7 @@
 //Distributed under the Boost Software License, Version 1.0. (See accompanying
 //file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-#include "boost/exception/info.hpp"
+#include <boost/exception/info.hpp>
 
 #ifdef BOOST_NO_CXX11_RVALUE_REFERENCES
 int
